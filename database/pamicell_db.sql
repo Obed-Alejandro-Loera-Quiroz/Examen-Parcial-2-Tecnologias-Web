6d3eedf6329ceb82ac8f0964221fdc5a0a31ec09
Obed Loera <obedloera21@gmail.com>
2026-04-13 18:14:38 -0600

    Colisones

--- database/pamicell_db.sql
+++ database/pamicell_db.sql
@@ -27,8 +27,8 @@ CREATE TABLE mensajes(
 --4 datos de prueba para el catalogo 
 INSERT INTO productos (nombre, categoria, marca, precio, stock, imagen, descripcion, disponible)
 VALUES
-('iphone 13', 'smartphones', 'Apple', 999.99, 50, 'iphone13.jpg', 'El iPhone 13 con pantalla Super Retina XDR y chip A15 Bionic.', 1),
+('Iphone 13', 'smartphones', 'Apple', 999.99, 50, 'iphone13.jpg', 'El iPhone 13 con pantalla Super Retina XDR y chip A15 Bionic.', 1),
 ('Galaxy S21', 'smartphones', 'Samsung', 799.99, 30, 'galaxy_s21.jpg', 'El Samsung Galaxy S21 con pantalla Dynamic AMOLED 2X y cámara de alta resolución.', 1),
 ('MacBook Pro', 'laptops', 'Apple', 1999.99, 20, 'macbook_pro.jpg', 'El MacBook Pro con chip M1 y pantalla Retina de alta resolución.', 1),
 ('Dell XPS 13', 'laptops', 'Dell', 1499.99, 15, 'dell_xps_13.jpg', 'El Dell XPS 13 con pantalla InfinityEdge y procesador Intel Core i7.', 1),
-('iPad Pro', 'tablets', 'Apple', 1099.99, 25, 'ipad_pro.jpg', 'El iPad Pro con pantalla Liquid Retina y chip M1 para un rendimiento excepcional.', 1);
+('IPad Pro', 'tablets', 'Apple', 1099.99, 25, 'ipad_pro.jpg', 'El iPad Pro con pantalla Liquid Retina y chip M1 para un rendimiento excepcional.', 1);
